00db595cd47defd3984d4ec38035fb70b86cf72a
Danilo Andres <daniloandresj@gmail.com>
2021-10-05 23:05:00 -0500

    Se añadio los inputs y botones

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
     return (
         <div>
-            cargo el login
+            <form  action="index.html">
+                    <input  type="email"  name="email" placeholder="Email" required/>
+                    <input  type="password" name="password" placeholder="Password" required/>
+                    <button type="submit">Sign In</button>
+            <div>
+                <label  htmlFor='Recuerdame'>
+                    <input type='checkbox' name='recuerdame'/>
+                    Recuerdame
+                </label>
+            </div>
+            <div>
+                <Link to='#'>
+                ¿Olvidaste tu contraseña?
+                </Link>
+            </div>
+            
+            
+            </form>
         </div>
     )
 }
