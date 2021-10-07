80525c53b63b39fc9175a89053e4380e2bd501f2
Danilo Andres <daniloandresj@gmail.com>
2021-10-06 22:35:41 -0500

    Se mejoro el diseño de la sub pagina productos y el background del contenido de PrivateLayout

--- src/layouts/PrivateLayout.jsx
+++ src/layouts/PrivateLayout.jsx
@@ -5,7 +5,7 @@ const PrivateLayout = ({children}) => {
     return (
         <div className='flex w-screen h-screen'>
             <Sidebar/>
-            <main className='flex w-full bg-blue-500 overflow-y-scroll items-center justify-center'> 
+            <main className='flex w-full bg-gray-900 overflow-y-scroll items-center justify-center'> 
             {children}
             </main>
         </div>
--- src/pages/admin/Productos.jsx
+++ src/pages/admin/Productos.jsx
@@ -2,39 +2,45 @@ import React , {useEffect, useState}  from 'react'
 
 const Productos = () => {
     
-    const [codigoProducto,setCodigoProducto] =useState('');
-    
+    const [valorDeProducto,setValorDeProducto] =useState(0);
+    const [sinDescuento,setSinDescuento]= useState(false);
     
     useEffect(()=>{
-        console.log('Hola soy unuseEffect que se inicia una vez solamnete porque el array esta vacio')
-    },[]);
-
-    const cambioDeNombre =(e)=>{
-        console.log(e.target.value);
-    };
+        if(valorDeProducto >70000){
+            setSinDescuento(false)
+            
+        }else{
+            setSinDescuento(true)
+        }
+    },[valorDeProducto])
+    
 
     const enviarDatosAlBackend =(e)=>{
-        console.log('El valor del codigo del producto es',codigoProducto)
+        console.log('El valor del producto es',valorDeProducto)
     };  
 
-    useEffect(()=>{
-        console.log("huy se cambio la variable socio")
-    },[codigoProducto])
-
+   
     return (
-        <form className='flex flex-col'>
-            <h2>Formulario de Creación de Productos</h2>
+        <form className='flex flex-col justify-center'>
+            <h2 className='py-2 text-gray-300 font-extrabold mb-4 text-center'>Formulario Creación de Productos</h2>
+             <input  type="text" className='appearance-none px-20  border  border-gray-400 rounded-md py-2 focus:outline-none' placeholder='Codigo del producto'/>
+            <input  type="text" className='appearance-none px-20 mt-4 border border-gray-400 rounded-md py-2 focus:outline-none' placeholder='Nombre del producto'/>
+            <input type="text" className='appearance-none px-20 mt-4 border border-gray-400 rounded-md py-2 focus:outline-none' placeholder='Marca del Producto'/>
 
-             <input onChange={(e)=>{
-                setCodigoProducto(e.target.value);
-             }} 
-             type="text" placeholder='Codigo del producto'/>
+            <input value={valorDeProducto} 
+            onChange={(e)=>{setValorDeProducto(e.target.value)}} 
+            type="number" placeholder='Valor del producto' className='appearance-none px-20 mt-4 border border-gray-400 rounded-md py-2 focus:outline-none'/>
 
-
-            <input onChange={cambioDeNombre} type="text" placeholder='Nombre del producto'/>
-            <input type="text" placeholder='Marca del Producto'/>
-            <input type="number" placeholder='Valor'/>
             <button type='button' onClick={enviarDatosAlBackend} className='mt-4 bg-green-500 px-36  rounded-md py-2 hover:bg-green-600'>Enviar Datos</button>
+            
+          
+            {sinDescuento ? (
+                    <span className='text-red-600 text-3xl mt-4'>Usted no recibio descuento</span>
+           
+                ):(
+                <span className='text-green-600 text-3xl mt-4'>Usted recibio descuento</span>
+                )}
+               
             
         </form>
     )
