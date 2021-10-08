2368cac00a9c602a7c7ebfc28acc65cd9fab6d9e
Danilo Andres <daniloandresj@gmail.com>
2021-10-08 16:24:17 -0500

    Cambio el estilo del privateLayout

--- src/layouts/PrivateLayout.jsx
+++ src/layouts/PrivateLayout.jsx
@@ -5,7 +5,7 @@ const PrivateLayout = ({children}) => {
     return (
         <div className='flex w-screen h-screen'>
             <Sidebar/>
-            <main className='flex w-full bg-gray-900 overflow-y-scroll items-center justify-center'> 
+            <main className='flex w-full bg-gray-700 overflow-y-scroll items-center justify-center'> 
             {children}
             </main>
         </div>
