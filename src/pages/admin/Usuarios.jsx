8e95a1d175357e3c394a65af3ebb0f318284826d
Danilo Andres <daniloandresj@gmail.com>
2021-10-09 14:36:47 -0500

    Mejoro el diseño de formulario de Usuarios del form

--- src/pages/admin/Usuarios.jsx
+++ src/pages/admin/Usuarios.jsx
@@ -35,14 +35,72 @@ const TablaUsuarios =()=>{
 
 const FormularioCreacionUsuarios =()=>{
     return <div>
-        <form action="" className='flex flex-col justify-center'>
-            <h2 className='py-2 text-gray-300 font-extrabold mb-4 text-center'>Creacion de Usuarios</h2>
-            <input  type="text" className='appearance-none px-20  border  border-gray-400 rounded-md py-2 focus:outline-none' placeholder='Codigo del Usuario'required/>
-            <input  type="text" className='appearance-none px-20 mt-4 border border-gray-400 rounded-md py-2 focus:outline-none' placeholder='Nombre del Usuario'required/>
-            <input type="text" className='appearance-none px-20 mt-4 border border-gray-400 rounded-md py-2 focus:outline-none' placeholder='Apellido del Usuario'required/>
-            <input type="email" className='appearance-none px-20 mt-4 border border-gray-400 rounded-md py-2 focus:outline-none' placeholder='Correo del Usuario'required/>
-            <input type="number" className='appearance-none px-20 mt-4 border border-gray-400 rounded-md py-2 focus:outline-none' placeholder='Cedula del Usuario'required/>
-            <input type="number" className='appearance-none px-20 mt-4 border border-gray-400 rounded-md py-2 focus:outline-none' placeholder='Telefono' required/>
+         <h2 className='py-2 text-gray-200 font-black mb-4 text-center'>Creacion de Usuarios</h2>
+        <form action="" className='grid grid-cols-1 justify-center'>
+           
+           
+           <label  htmlFor="codigo" className='text-gray-200 font-extrabold'>
+               Codigo del Usuario
+            <input 
+             name='codigo'   
+             type="text" 
+             className='appearance-none px-16  border  border-gray-300 rounded-md py-2 ml-6 text-center focus:outline-none' 
+             placeholder='Codigo del Usuario'
+             required
+             />
+            </label>
+            
+            <label htmlFor="nombre" className='text-gray-200 font-extrabold '>
+                Nombre del Usuario
+            <input  
+            name='nombre'
+            type="text" 
+            className='appearance-none px-16 mt-4 border border-gray-100 rounded-md py-2 ml-4 text-center focus:outline-none' 
+            placeholder='Nombre del Usuario'
+            required/>
+            </label>
+
+            <label htmlFor="apellido" className='text-gray-200 font-extrabold '> 
+            Apellido del Usuario
+            <input 
+            name='apellido'
+            type="text" 
+            className='appearance-none px-16 mt-4 border border-gray-400 rounded-md py-2 ml-4 text-center  focus:outline-none' 
+            placeholder='Apellido del Usuario'
+            required/>
+            </label>
+
+            <label htmlFor="email" className='text-gray-200 font-extrabold '>
+            Email del Usuario
+            <input 
+            name='email'
+            type="email" 
+            className='appearance-none px-16 mt-4 border border-gray-400 rounded-md py-2 ml-10 text-center  focus:outline-none' 
+            placeholder='Correo del Usuario'
+            required/>
+            </label>
+
+
+            <label htmlFor="cedula" className='text-gray-200 font-extrabold '>
+                Cedula del Usuario
+            <input 
+            name='cedula'
+            type="number" 
+            className='appearance-none px-16 mt-4 border border-gray-400 rounded-md py-2 ml-7 text-center  focus:outline-none' 
+            placeholder='Cedula del Usuario'
+            required/>
+            </label>
+            
+            <label htmlFor="telefono" className='text-gray-200 font-extrabold '>
+            Telefono del Usuario
+            <input 
+            name='telefono'
+            type="text" 
+            className='appearance-none px-16 mt-4 border border-gray-400 rounded-md py-2 ml-3 text-center  focus:outline-none' 
+            placeholder='Telefono' 
+            required/>
+            </label>
+
             <button type='submit' className='mt-4 bg-green-500 px-36  rounded-md py-2 hover:bg-green-600'>Enviar Datos</button>
         </form>
     </div>
