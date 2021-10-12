f88b72be087e60be3c4f95aeabb35638e3869322
Danilo Andres <daniloandresj@gmail.com>
2021-10-11 20:34:08 -0500

    se importo imagen logo

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
+
+
+
+
+
 const Sidebar = () => {
     return (
         <div className='w-72 bg-gray-800'>
-            Contenido Sidebar
+            
+         
+            <button>Administración de vehiculos</button>
+           
         </div>
     )
 }
---
+++ src/components/imagenLogo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Logo from 'media/logo2r.svg'
+const imagenLogo = () => {
+   
+   
+    return <div>
+        <img src={Logo} alt="" />
+    </div>;
+        
+    
+};
+
+export default imagenLogo;
