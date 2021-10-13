ec400122653241c5d2952a34f13f741d96df20b2
Danilo Andres <daniloandresj@gmail.com>
2021-10-13 18:53:57 -0500

    En el index.jsx de la carpeta public se añadio el script para usar el kit de iconos de fontawesome, se añadio los dos iconos para animar el menu despelgable

--- src/components/SideBarResponsive.jsx
+++ src/components/SideBarResponsive.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import { useState } from 'react/cjs/react.development'
 
+
+
 const SideBarResponsive = () => {
     const [mostrarNavegacion,setMostrarNavegacion] =useState(false)
     return (
-        <div className='md:hidden' onClick={()=>{setMostrarNavegacion(!mostrarNavegacion)}}>
-            <svg class="h-8 w-8 text-black"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 10h16M4 14h16M4 18h16"/>
-            </svg>
+        <div className='md:hidden' 
+        onClick={()=>{
+            setMostrarNavegacion(!mostrarNavegacion)
+            }}>
+                <i className={`fas fa-${mostrarNavegacion ? 'times': 'bars'} hover:text-yellow-600`} />
+
        </div>
     )
 }
+
+
 
 export default SideBarResponsive
