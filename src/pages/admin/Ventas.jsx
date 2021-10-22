3046e712e297344751ced17686dba8b8593fdb3c
Danilo Andres <daniloandresj@gmail.com>
2021-10-21 21:32:54 -0500

    se creo vendedores y el crud de productos

--- src/pages/admin/Ventas.jsx
+++ src/pages/admin/Ventas.jsx
@@ -1,12 +1,49 @@
 import React from 'react'
+import { useState,useEffect } from 'react'
+import { obtenerUsuarios } from 'utils/api'
 
 
 const Ventas = () => {
+     const [vendedor, setVendedores] = useState([])
+     const [producto,setproducto] =useState([])
+     
+     
+     useEffect(() => {
+          const fetchVendedores = async()=>{
+               await obtenerUsuarios(
+                    (response)=>{
+                         setVendedores(response.data)
+                    },
+                    (error)=>{console.error(error)}
+                    )
+          }
+
+          const fetchProductos = async () =>{
+               
+          }
+         
+          fetchVendedores()
+          fetchProductos()
+          
+     }, [])
+
+
   return (
        <div>
-        <h2>Pagina de administración de ventas</h2>
+        <form className='flex flex-col'>
+             <label className='flex flex-col' htmlFor='vendedor'>
+                  <span className='text-2xl font-gray-900'>Vendedor</span>
+             <select name="vendedor" className='p-2' defaultValue={-1}>
+                  <option disabled value={-1} >
+                       Seleccione un vendedor
+                  </option>
+                  {vendedor.map((el)=>{
+                     return <option value={el._id} >{`${el.name} ${el.lastName}`}</option>;
+                      })}
+             </select>
+             </label>
+        </form>
         </div>
-
    )
 }
 
--- src/utils/api.js
+++ src/utils/api.js
@@ -39,3 +39,47 @@ export const eliminarUsuarios = async (id,successCallback,errorCallback)=>{
     }
     await axios.request(options).then(successCallback).catch(errorCallback);
 }
+
+///////////////////////////----------------------------------------------
+
+export const obtenerProductos = async(successCallback,errorCallback)=>{
+    const options ={method: 'GET', url: 'http://localhost:5000/productos/' };
+    await axios.request(options).then(successCallback).catch(errorCallback)
+}
+
+export const crearProductos = async(data, successCallback,errorCallback)=>{
+    const options ={
+        
+            method: 'POST',
+            url:'http://localhost:5000/productos/',
+            headers: { 'Content-Type': 'application/json' },
+            data
+        
+    }
+    await axios.request(options).then(successCallback).catch(errorCallback);
+
+}
+
+
+export const editarProductos = async (id,data,successCallback,errorCallback)=>{
+    const options ={
+            method: 'PATCH',
+            url: `http://localhost:5000/productos/${id}/`,
+            headers: { 'Content-Type': 'application/json' },
+            data,
+    }
+    await axios.request(options).then(successCallback).catch(errorCallback);
+}
+
+
+export const eliminarProductos = async (id,successCallback,errorCallback)=>{
+
+    const options ={
+        method: 'DELETE',
+        url:`http://localhost:5000/productos/${id}/`,
+        headers: { 'Content-Type': 'application/json' }
+    }
+    await axios.request(options).then(successCallback).catch(errorCallback);
+}
+
+
