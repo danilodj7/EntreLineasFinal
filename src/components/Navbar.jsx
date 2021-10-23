904c1a38e8c09b5f206f8ac3ee01da772b543db9
Danilo Andres <daniloandresj@gmail.com>
2021-10-22 20:15:36 -0500

    Se creo la auth de boton login

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import TriggerDarkMode from './TriggerDarkMode'
+import { useAuth0 } from "@auth0/auth0-react";
 
 const NavBar = () => {
-
+    const { loginWithRedirect } = useAuth0();
     return (
         <div className='bg-gray-900' >
             <ul className='grid grid-cols-6  text-gray-50 m-4' >
@@ -12,11 +13,13 @@ const NavBar = () => {
                 <li>Productos</li>
                 <li>Quienes Somos</li>
                 <TriggerDarkMode/>
-                <Link to='/login'>
+               
                 <li  >
-                    <button className='bg-gray-800 p-2 rounded-lg  hover:bg-blue-400 hover:text-gray-900'>Iniciar sesion</button>
+                    <button 
+                     onClick={() => loginWithRedirect()}
+                    className='bg-gray-800 p-2 rounded-lg  hover:bg-blue-400 hover:text-gray-900'>Iniciar sesion</button>
                 </li>
-                </Link>
+              
                 <Link to='/registro'>
                 <li>
                 <button className='bg-gray-800 p-2 rounded-lg  hover:bg-blue-400 hover:text-gray-900'>Registrarse</button>
